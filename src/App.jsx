8f07a5935a066245990fec68ef847e87961b0a62
Shah Ehsan <shahehsan2002@gmail.com>
2024-10-03 00:37:28 +0600

    clear some concept

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,49 @@
+import { OrbitControls, Sparkles } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
+
+const RotatingCube = () => {
+  // const meshRef:MutableRefObject<undefined> = useRef();
+  const meshRef = useRef();
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.y += 0.01;
+      meshRef.current.rotation.x += 0.01;
+    }
+  });
+  return (
+    <mesh ref={meshRef}>
+      <cylinderGeometry args={[1, 1, 1]} />
+      <meshLambertMaterial color="#468585" emissive="#468585" />
+
+      <Sparkles count={100} scale={1} size={8} speed={0.002} noise={0.2} />
+    </mesh>
+  );
+};
 const App = () => {
   return (
-    <div >
-      <h1>Hello, World!</h1>
-    </div>
-  )
-}
+    <Canvas
+      style={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <OrbitControls enableZoom enablePan enableRotate />
 
-export default App
+      <directionalLight
+        position={[-1, -1, -1]}
+        intensity={10}
+        color={0x9cdba6}
+      />
+
+      <color attach="background" args={["#F0F0F0"]} />
+
+      <RotatingCube />
+    </Canvas>
+  );
+};
+
+export default App;
